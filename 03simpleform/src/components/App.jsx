f6ea0660c4ad186c11d92e0c1443fabe42150296
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-08-26 21:20:50 +0530

    most of the part done some work is left to do

---
+++ 03simpleform/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function submited(event) {
+    setHeadingText(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={submited}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
